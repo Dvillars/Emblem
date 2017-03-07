99d8809c3e0f6db168e3b26c794b9c1e0a618c44
Marc Larkin <larkimarc@gmail.com>
2017-03-07 15:34:03 -0800

    add index.cshtml and layout

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -3,13 +3,16 @@ using Nancy;
 using System;
 using Nancy.ViewEngines.Razor;
 
-namespace Cookbook
+namespace SigilOfFlame
 {
     public class HomeModule : NancyModule
     {
         public HomeModule()
         {
-
+            Get["/"] = _ => {
+            List<Unit> allUnits = Unit.GetAll();
+            return View["index.cshtml", allUnits];
+        };
         }
     }
 }
