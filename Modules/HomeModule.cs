22b5346f17248370be9d7791920b38e21b0ff127
Marc Larkin <larkimarc@gmail.com>
2017-03-08 14:25:08 -0800

    add a big dictionary to store everything

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -7,12 +7,60 @@ namespace SigilOfFlame
 {
     public class HomeModule : NancyModule
     {
+        public static List<Unit> allUnits = Unit.GetAll();
+        public static List<Player> allPlayers = Player.GetAll();
+        public static List<Weapon> allWeapons = Weapon.GetAll();
+
+        public static Player playerOne = new Player(Request.Form["player-one-name"]);
+        public static Player playerTwo = new Player(Request.Form["player-two-name"]);
+
+        public static Dictionary<string, object> allThings = new Dictionary<string, object>()
+        {
+
+            {"p1", Dictionary<string, object> playerOneInformation = new Dictionary<string, object>()
+            {
+                {"name", playerOne.GetName()},
+                {"units", playerOne.GetUnits()}
+            };
+            },
+
+
+            {"p2", Dictionary<string, object> playerTwoInformation = new Dictionary<string, object>()
+            {
+                {"name", playerTwo.GetName()},
+                {"units", playerTwo.GetUnits()}
+
+            };
+            },
+
+            {"allUnits", allUnits},
+            {"allPlayers", allPlayers},
+            {"allWeapons", allWeapons}
+        };
         public HomeModule()
         {
             Get["/"] = _ => {
-            List<Unit> allUnits = Unit.GetAll();
-            return View["index.cshtml", allUnits];
-        };
+                return View["index.cshtml", allUnits];
+            };
+
+            Post["/ready-check"] = _ => {
+
+            };
+
+            Get["/arena"] = _ => {
+
+                List<Unit> allPlayerUnits = new List<Unit>{};
+                foreach (var player in allPlayers)
+                {
+                    List<Unit> playerUnits = this.GetUnits();
+                    allUnits.Add(playerUnits);
+                }
+
+
+
+
+
+            };
         }
     }
 }
